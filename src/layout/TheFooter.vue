0042e31f7b8353e54bd1418e2bf8a47616435bef
minjiwon <mjo4260@naver.com>
2025-05-31 22:57:36 +0900

    feat: Footer 구현

---
+++ src/layout/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-footer app class="pa-3 bg-grey-lighten-3">
+		<v-container class="d-flex justify-space-between align-center">
+			<div>
+				&copy; {{ new Date().getFullYear() }} Ready Auction - 개인 프로젝트
+			</div>
+
+			<v-btn
+				icon
+				href="https://github.com/f-lab-edu/ready-auction"
+				target="_blank"
+				rel="noopener"
+				aria-label="GitHub Repository"
+			>
+				<v-icon>mdi-github</v-icon>
+			</v-btn>
+		</v-container>
+	</v-footer>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
